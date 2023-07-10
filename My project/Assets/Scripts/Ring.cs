3e6274bbcd0a8b7f5dbbbe1bea2b4185ab1df692
CorentinDebrabant <debrabantcorentin@gmail.com>
2023-07-10 16:17:11 +0200

    Création de la scène de jeu
    -Création d'une scène
    -Création de préfabs pour la planète, l'anneau et la tourelle mobile
    -Scripts pour la rotation de la planète, du soleil et pour le contrôle de l'anneau

---
+++ My project/Assets/Scripts/Ring.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("left"))
+        {
+            transform.Rotate(0, 0, 0.5f);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(0, 0, -0.5f);
+        }
+    }
+}
